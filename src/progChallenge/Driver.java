0edfcd7a6b4459e07eb825573b57f96f37858ce7
HunterEl <elbournhr@beloit.edu>
2014-05-02 16:51:49 -0500

    Added class
    created the driver class which I will be working on.

---
+++ src/progChallenge/Driver.java
@@ -0,0 +1,16 @@
+package progChallenge;
+/**
+ * @author Joe Peacock
+ * @author Hunter Elbourn
+ * last edited 5/2/14
+ * TODO: Driver class/ Hash Map (collections class)/ mergesort/ finish linked list class
+ *
+ */
+
+public class Driver {
+
+	public static void main(String args[]) {
+		//will fill in later
+	}
+
+}
